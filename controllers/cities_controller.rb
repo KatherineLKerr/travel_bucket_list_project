992f26a00aa0a98188ff023004988590bbf6fd56
Katherine Kerr <katherinekerr@protonmail.com>
2019-06-09 20:16:57 +0100

    added confirmation page, add city working

--- controllers/cities_controller.rb
+++ controllers/cities_controller.rb
@@ -4,8 +4,14 @@ require( 'pry-byebug' )
 require_relative("../models/city.rb")
 also_reload("../models/*")
 
-get '/cities/new' do
+get '/cities/new' do #new
   @countries = Country.all
   @cities = City.all
   erb(:"cities/new")
+end
+
+post "/index" do #create
+  new_city = City.new(params)
+  new_city.save
+  erb(:confirmation)
 end
