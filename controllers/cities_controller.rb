df31d3558b00e95417e23baed1448d95314bddb6
Katherine Kerr <katherinekerr@protonmail.com>
2019-06-09 16:50:00 +0100

    fixed routes for new city and new country

--- controllers/cities_controller.rb
+++ controllers/cities_controller.rb
@@ -1,4 +1,9 @@
 require("sinatra")
 require("sinatra/contrib/all")
+require( 'pry-byebug' )
 require_relative("../models/city.rb")
 also_reload("../models/*")
+
+get '/cities/new' do
+  erb(:"cities/new")
+end
--- controllers/countries_controller.rb
+++ controllers/countries_controller.rb
@@ -1,4 +1,9 @@
 require("sinatra")
 require("sinatra/contrib/all")
+require( 'pry-byebug' )
 require_relative("../models/country.rb")
 also_reload("../models/*")
+
+get '/countries/new' do
+  erb(:"countries/new")
+end
