f6ec22b93512a9e11c55c1f7c7f46f56cc24793c
Katherine Kerr <katherinekerr@protonmail.com>
2019-06-09 12:06:54 +0100

    adds list of countries to your_bucket_list.erb

--- bucket_list_controller.rb
+++ bucket_list_controller.rb
@@ -3,10 +3,16 @@ require("sinatra/contrib/all")
 require_relative('controllers/cities_controller')
 require_relative('controllers/countries_controller')
 
+
 get "/" do
   erb(:index)
 end
 
 get "/about" do
   erb(:about)
+end
+
+get "/your_bucket_list" do
+  @countries = Country.all
+  erb(:your_bucket_list)
 end
