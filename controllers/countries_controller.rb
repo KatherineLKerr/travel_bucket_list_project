b04c54d9efa78667d70f21194ba999d9343f07f1
Katherine Kerr <katherinekerr@protonmail.com>
2019-06-10 11:17:51 +0100

    link to edit country working

--- controllers/countries_controller.rb
+++ controllers/countries_controller.rb
@@ -4,9 +4,20 @@ require( 'pry-byebug' )
 require_relative("../models/country.rb")
 also_reload("../models/*")
 
+get "/countries" do #index - countries
+  @countries = Country.all
+  erb(:countries)
+end
+
 get '/countries/new' do #new
   @countries = Country.all
   erb(:"countries/new")
+end
+
+get "/countries/:id" do #show
+  @cities = City.all
+  @country = Country.find_by_id(params[:id])
+  erb(:"countries/show")
 end
 
 post "/countries" do #create
@@ -14,3 +25,19 @@ post "/countries" do #create
   new_country.save
   erb(:confirmation)
 end
+
+get "/countries/:id/edit" do #edit
+  @country = Country.find_by_id(params[:id])
+  erb(:"countries/edit")
+end
+
+
+##FOR COUNTRIES
+#
+# index  = get  "/countries"
+# show   = get  "/countries/:id"
+# new    = get  "/countries/new"
+# create = post "/countries"
+# edit   = get  "/countries/:id/edit"
+# update = post "/countries/:id"
+# destroy= post "/countries/id/delete"
